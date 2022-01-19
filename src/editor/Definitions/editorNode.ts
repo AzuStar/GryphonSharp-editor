4ad07f3af2f15e7ed9180e30142b17de11ea9d0a
Azureus <Azureus0@icloud.com>
2022-01-19 15:43:09 +0000

    NE node wrapper

--- src/editor/Definitions/editorNode.ts
+++ src/editor/Definitions/editorNode.ts
@@ -1,3 +1,14 @@
-export class NENode{
+export class NENode {
+    public readonly konvaId: string;
+    public readonly numbericId : number;
+    constructor(nodeId: number) {
+        this.konvaId = "node-"+nodeId;
+        this.numbericId = nodeId;
+    }
+    // /**
+    //  * UpdateState
+    //  */
+    // public UpdatePosition(): void {
 
+    // }
 }
